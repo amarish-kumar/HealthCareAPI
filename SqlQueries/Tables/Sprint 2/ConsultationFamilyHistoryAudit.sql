ccb2ed6304ce9e9e664a1a313f644fa903e7ed14
Kunal <kunalsmehtajobs@gmail.com>
2018-03-12 18:37:22 +0530

    Added script files for OMC-118

---
+++ SqlQueries/Tables/Sprint 2/ConsultationFamilyHistoryAudit.sql
@@ -0,0 +1,34 @@
+USE [HealthCare]
+GO
+
+/****** Object:  Table [dbo].[ConsultationFamilyHistoryAudit]    Script Date: 3/12/2018 6:35:30 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ConsultationFamilyHistoryAudit](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Action] [nvarchar](10) NOT NULL,
+	[EntityId] [bigint] NOT NULL,
+	[ConsultationId] [bigint] NOT NULL,
+	[RelationshipId] [bigint] NOT NULL,
+	[HealthConditionId] [bigint] NOT NULL,
+	[CurrentAge] [int] NULL,
+	[AgeOnConditionStart] [int] NULL,
+	[IsAlive] [bit] NOT NULL,
+	[CAuseOfDeath] [nvarchar](255) NULL,
+	[AgeOnDeath] [int] NULL,
+	[Active] [bit] NOT NULL,
+	[AddedBy] [bigint] NULL,
+	[AddedDate] [datetime] NULL,
+	[ModifiedBy] [bigint] NULL,
+	[ModifiedDate] [datetime] NULL,
+	[DeletedBy] [bigint] NULL,
+	[DeletedDate] [datetime] NULL,
+ CONSTRAINT [PK_ConsultationFamilyHistoryAudit] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
