7542606f8e22536ad3b9bfff5f16d6861a55a1a5
Kunal <kunalsmehtajobs@gmail.com>
2018-03-12 19:17:20 +0530

    Updated script files for OMC-120 to include OMC-162

--- SqlQueries/Tables/Sprint 2/ConsultationSDDHabits.sql
+++ SqlQueries/Tables/Sprint 2/ConsultationSDDHabits.sql
@@ -1,17 +1,7 @@
-USE [HealthCare]
-GO
 
-/****** Object:  Table [dbo].[ConsultationSDHabitsAudit]    Script Date: 3/12/2018 6:47:06 PM ******/
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[ConsultationSDHabitsAudit](
+CREATE TABLE [dbo].[ConsultationSDDHabits](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
-	[Action] [nvarchar](10) NOT NULL,
-	[EntityId] [bigint] NOT NULL,
 	[ConsultationId] [bigint] NOT NULL,
 	[ConsumeAlcohol] [bit] NOT NULL,
 	[AlcoholConsumptionFreq] [nvarchar](15) NULL,
@@ -21,6 +11,10 @@ CREATE TABLE [dbo].[ConsultationSDHabitsAudit](
 	[EverSmoked] [bit] NOT NULL,
 	[YearOfQuittingSmoking] [int] NULL,
 	[SmokingFreq] [int] NULL,
+	[ConsumeDrugs] [bit] NOT NULL,
+	[DrugsConsumptionFreq] [nvarchar](15) NULL,
+	[DrugsPerDay] [int] NULL,
+	[DrugsPerWeek] [int] NULL,
 	[Active] [bit] NOT NULL,
 	[AddedBy] [bigint] NULL,
 	[AddedDate] [datetime] NULL,
@@ -28,11 +22,19 @@ CREATE TABLE [dbo].[ConsultationSDHabitsAudit](
 	[ModifiedDate] [datetime] NULL,
 	[DeletedBy] [bigint] NULL,
 	[DeletedDate] [datetime] NULL,
- CONSTRAINT [PK_ConsultationSDHabitsAudit] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_ConsultationSDDHabits] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+
+ALTER TABLE [dbo].[ConsultationSDDHabits]  WITH CHECK ADD  CONSTRAINT [FK_ConsultationSDDHabits_Consultation_Id] FOREIGN KEY([ConsultationId])
+REFERENCES [dbo].[Consultation] ([Id])
+GO
+
+ALTER TABLE [dbo].[ConsultationSDDHabits] CHECK CONSTRAINT [FK_ConsultationSDDHabits_Consultation_Id]
+GO
 
 
--- SqlQueries/Tables/Sprint 2/ConsultationSDDHabitsAudit.sql
+++ SqlQueries/Tables/Sprint 2/ConsultationSDDHabitsAudit.sql
@@ -1,24 +1,30 @@
 USE [HealthCare]
 GO
 
-/****** Object:  Table [dbo].[ConsultationSDHabits]    Script Date: 3/12/2018 6:42:07 PM ******/
+/****** Object:  Table [dbo].[ConsultationSDDHabitsAudit]    Script Date: 3/12/2018 6:47:06 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[ConsultationSDHabits](
+CREATE TABLE [dbo].[ConsultationSDDHabitsAudit](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Action] [nvarchar](10) NOT NULL,
+	[EntityId] [bigint] NOT NULL,
 	[ConsultationId] [bigint] NOT NULL,
 	[ConsumeAlcohol] [bit] NOT NULL,
-	[AlcoholConsumptionFreq] nvarchar(15) NULL,
+	[AlcoholConsumptionFreq] [nvarchar](15) NULL,
 	[DrinksPerDay] [int] NULL,
 	[DrinksPerWeek] [int] NULL,
 	[DoSmoke] [bit] NOT NULL,
 	[EverSmoked] [bit] NOT NULL,
 	[YearOfQuittingSmoking] [int] NULL,
 	[SmokingFreq] [int] NULL,
+	[ConsumeDrugs] [bit] NOT NULL,
+	[DrugsConsumptionFreq] [nvarchar](15) NULL,
+	[DrugsPerDay] [int] NULL,
+	[DrugsPerWeek] [int] NULL,
 	[Active] [bit] NOT NULL,
 	[AddedBy] [bigint] NULL,
 	[AddedDate] [datetime] NULL,
@@ -26,17 +32,11 @@ CREATE TABLE [dbo].[ConsultationSDHabits](
 	[ModifiedDate] [datetime] NULL,
 	[DeletedBy] [bigint] NULL,
 	[DeletedDate] [datetime] NULL,
- CONSTRAINT [PK_ConsultationSDHabits] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_ConsultationSDDHabitsAudit] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
-GO
 
-ALTER TABLE [dbo].[ConsultationSDHabits]  WITH CHECK ADD  CONSTRAINT [FK_ConsultationSDHabits_Consultation_Id] FOREIGN KEY([ConsultationId])
-REFERENCES [dbo].[Consultation] ([Id])
-GO
 
-ALTER TABLE [dbo].[ConsultationSDHabits] CHECK CONSTRAINT [FK_ConsultationSDHabits_Consultation_Id]
-GO
